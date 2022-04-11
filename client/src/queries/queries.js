d93991f15e57e64097a081dd685df3cc42c75047
Harshitha230 <51833141+Harshitha230@users.noreply.github.com>
2022-04-11 17:09:05 +0530

    Update queries.js

--- client/src/queries/queries.js
+++ client/src/queries/queries.js
@@ -1,5 +1,6 @@
 import { gql } from '@apollo/client'
 
+//Retrieve all the products stored in the database
 export const ALL_PRODUCTS = gql`
   query allProducts {
     products {
@@ -10,7 +11,7 @@ export const ALL_PRODUCTS = gql`
     }
   }
 `
-
+//Retrieve a particular product
 export const GET_PRODUCT = gql`
   query getProduct($id: ID!) {
     product(id: $id) {
@@ -21,7 +22,7 @@ export const GET_PRODUCT = gql`
     }
   }
 `
-
+//Create a new product
 export const CREATE_PRODUCT = gql`
   mutation createProduct($name: String!, $price: Float!, $quantity: Float!) {
     createProduct(name: $name, price: $price, quantity: $quantity) {
@@ -32,7 +33,7 @@ export const CREATE_PRODUCT = gql`
     }
   }
 `
-
+//Update an existing product
 export const UPDATE_PRODUCT = gql`
   mutation updateProduct(
     $id: ID!
@@ -48,7 +49,7 @@ export const UPDATE_PRODUCT = gql`
     }
   }
 `
-
+//Delete an existing product
 export const DELETE_PRODUCT = gql`
   mutation deleteProduct($id: ID!) {
     deleteProduct(id: $id) {
@@ -59,7 +60,7 @@ export const DELETE_PRODUCT = gql`
     }
   }
 `
-
+//Register a new user into the database
 export const USER_SIGNUP = gql`
   mutation UserSignUp($name: String!, $email: String!, $password: String!) {
     userSignUp(name: $name, email: $email, password: $password) {
@@ -70,7 +71,7 @@ export const USER_SIGNUP = gql`
     }
   }
 `
-
+//Login an existing user
 export const USER_LOGIN = gql`
   mutation userLogin($email: String!, $password: String!) {
     userLogin(email: $email, password: $password) {
